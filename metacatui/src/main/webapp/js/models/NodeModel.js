9dd3f6e14dd61eedd731c5f8939cc5a2061e59b0
lauren-palmer <walker@nceas.ucsb.edu>
2016-07-13 07:32:09 -0700

    Add new and upcoming NEON and TDAR node logos

--- metacatui/src/main/webapp/js/models/NodeModel.js
+++ metacatui/src/main/webapp/js/models/NodeModel.js
@@ -43,7 +43,9 @@ define(['jquery', 'underscore', 'backbone'],
 				"urn:node:PPBIO"       : "img/node-logos/PPBIO.png",
 				"urn:node:DFC"         : "img/node-logos/DFC.png",
 				"urn:node:BCODMO"      : "img/node-logos/BCODMO.jpg",
-				"urn:node:RD2R"        : "img/node-logos/RD2R.png"				
+				"urn:node:RD2R"        : "img/node-logos/RD2R.png",				
+				"urn:node:NEON"        : "img/node-logos/NEON.png",				
+				"urn:node:TDAR"        : "img/node-logos/TDAR.png"				
 		},
 		
 		// This model contains all of the information retrieved from calling listNodes() using the DataONE API
