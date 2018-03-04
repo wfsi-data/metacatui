56783af4887df1c1b5de5aabda7a5e2feb234d9b
Christopher Jones <csj@christopherjones.co>
2018-03-03 17:06:50 -0700

    Migrate theme format to JSON:
    Copy each theme's config.js file to config.json, and reformat the JSON
    files to be JSON -compliant (no JS code or comments).
    Remove config.js files for themes that have no JS code needed to
    customize the app. For themes with custom JS code, modify the config.js
    files to remove the static config lines (now in the JSON), and keep the
    JS code.  We will plan in a standard  app customization pattern and call
    this code with that mechanism (TODO).
    
    refs #509

--- src/js/themes/arctic/config.js
+++ src/js/themes/arctic/config.js
@@ -1,66 +1,37 @@
-MetacatUI.theme = MetacatUI.theme || "arctic";
-MetacatUI.themeTitle = "NSF Arctic Data Center";
-MetacatUI.themeMap = 
-{
-	'*': {
-		// Templates include extension
-		'templates/navbar.html' : 'themes/' + MetacatUI.theme + '/templates/navbar.html',
-		'templates/featuredData.html' : 'themes/' + MetacatUI.theme + '/templates/featuredData.html',
-		'templates/footer.html' : 'themes/' + MetacatUI.theme + '/templates/footer.html',
-		'templates/mainContent.html' : 'themes/' + MetacatUI.theme + '/templates/mainContent.html',
-		'templates/altHeader.html' : 'themes/' + MetacatUI.theme + '/templates/altHeader.html',
-		'templates/defaultHeader.html' : 'themes/' + MetacatUI.theme + '/templates/defaultHeader.html',
-		'templates/userProfileMenu.html' : 'themes/' + MetacatUI.theme + '/templates/userProfileMenu.html',
-		'templates/publishDOI.html' : 'themes/' + MetacatUI.theme + '/templates/publishDOI.html',
-		'templates/resultsItem.html' : 'themes/' + MetacatUI.theme + '/templates/resultsItem.html',
-		'templates/noResults.html' : 'themes/' + MetacatUI.theme + '/templates/noResults.html',
-		'templates/loginButtons.html' : 'themes/' + MetacatUI.theme + '/templates/loginButtons.html',
-		'templates/metadata.html' : 'themes/' + MetacatUI.theme + '/templates/metadata.html',
-		'templates/insertProgress.html' : 'themes/' + MetacatUI.theme + '/templates/insertProgress.html',
-		'models/AppModel' : 'js/themes/' + MetacatUI.theme + '/models/AppModel.js',
-		'models/Map' : 'js/themes/' + MetacatUI.theme + '/models/Map.js',
-		'models/Search' : 'js/themes/' + MetacatUI.theme + '/models/Search.js',
-		'routers/router' : 'js/themes/' + MetacatUI.theme + '/routers/router.js'
-		}
-};
-
 MetacatUI.customMapModelOptions = {
-	tileHue: "231"
+    tileHue: "231"
 }
 
-MetacatUI.customAppConfig = function(){	
-	//Gmaps key: AIzaSyCYoTkUEpMAiOoWx5M61ButwgNGX8fIHUs
-	
-	//Check that slaask didn't fail before getting its dependency, Pusher
-	/*if(window._slaask){
-		//Override _slaask.createScriptTag to use requireJS to load injected module 'Pusher'
-	    window._slaask.createScriptTag = function (url) {
-	        var t = {};
-	        require([url], function(Pusher) { 
-	        	t.onload(); 
-	        	});
-	        return t;
-	    };
-	}
-	*/
-	if(MetacatUI.appModel.get("baseUrl").indexOf("arcticdata.io") > -1 && MetacatUI.appModel.get("baseUrl").indexOf("test") == -1){
-		MetacatUI.appModel.set("nodeId", "urn:node:ARCTIC");
-		MetacatUI.appModel.set("googleAnalyticsKey", "UA-75482301-1");
-	}
+MetacatUI.customAppConfig = function() {
+    // Gmaps key: AIzaSyCYoTkUEpMAiOoWx5M61ButwgNGX8fIHUs
+    
+    // Check that slaask didn"t fail before getting its dependency, Pusher
+    // if(window._slaask){
+    //     //Override _slaask.createScriptTag to use requireJS to load injected module "Pusher"
+    //     window._slaask.createScriptTag = function (url) {
+    //         var t = {};
+    //         require([url], function(Pusher) { 
+    //             t.onload(); 
+    //             });
+    //         return t;
+    //     };
+    // }
+    if(MetacatUI.appModel.get("baseUrl").indexOf("arcticdata.io") > -1 && MetacatUI.appModel.get("baseUrl").indexOf("test") == -1){
+        MetacatUI.appModel.set("nodeId", "urn:node:ARCTIC");
+        MetacatUI.appModel.set("googleAnalyticsKey", "UA-75482301-1");
+    }
 }
-
-/*
-var customInitApp = function(){
-	var slaaskScript = document.createElement("script");
-	slaaskScript.setAttribute("type", "text/javascript");
-	slaaskScript.setAttribute("src",  "https://cdn.slaask.com/chat.js");
-	document.getElementsByTagName("body")[0].appendChild(slaaskScript);
-	
-	//Give the slaask script 3 seconds to load or move on without it!
-	var slaaskTimeout = window.setTimeout(function(){
-		initApp();	
-		
-		//Don't check again
-		window.clearTimeout(slaaskTimeout);
-	}, 3000);
-}*/
+// var customInitApp = function(){
+//     var slaaskScript = document.createElement("script");
+//     slaaskScript.setAttribute("type", "text/javascript");
+//     slaaskScript.setAttribute("src",  "https://cdn.slaask.com/chat.js");
+//     document.getElementsByTagName("body")[0].appendChild(slaaskScript);
+//
+//     //Give the slaask script 3 seconds to load or move on without it!
+//     var slaaskTimeout = window.setTimeout(function(){
+//         initApp();
+//
+//         //Don"t check again
+//         window.clearTimeout(slaaskTimeout);
+//     }, 3000);
+// }
--- src/js/themes/goa/config.js
+++ src/js/themes/goa/config.js
@@ -1,16 +1,14 @@
-MetacatUI.theme = MetacatUI.theme || "goa";
-MetacatUI.themeTitle = "Gulf of Alaska Data Portal";
-MetacatUI.themeMap = 
 {
-	'*': {
-		// example overrides are provided here
-		//'views/AboutView' : 'themes/' + MetacatUI.theme + '/views/AboutView.js',
-		'templates/app.html' : 'themes/' + MetacatUI.theme + '/templates/app.html',
-		'templates/navbar.html' : 'themes/' + MetacatUI.theme + '/templates/navbar.html',
-		'templates/footer.html' : 'themes/' + MetacatUI.theme + '/templates/footer.html',
-		'templates/search.html' : 'themes/' + MetacatUI.theme + '/templates/search.html',
-		'templates/defaultHeader.html' : 'themes/' + MetacatUI.theme + '/templates/defaultHeader.html',
-		'routers/router' : 'themes/' + MetacatUI.theme + '/routers/router'
-		//'templates/resultsItem.html' : 'themes/' + MetacatUI.theme + '/templates/resultsItem.html'
-		}
-};
+    "themeName": "goa",
+    "themeTitle": "Gulf of Alaska Data Portal",
+    "moduleMap": {
+        "*": {
+            "templates/app.html": "themes/goa/templates/app.html",
+            "templates/navbar.html": "themes/goa/templates/navbar.html",
+            "templates/footer.html": "themes/goa/templates/footer.html",
+            "templates/search.html": "themes/goa/templates/search.html",
+            "templates/defaultHeader.html": "themes/goa/templates/defaultHeader.html",
+            "routers/router": "themes/goa/routers/router"
+        }
+    }
+}
