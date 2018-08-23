e2995d04646d083b4069a0d5491faa938fc96872
Christopher Jones <csj@christopherjones.co>
2018-08-23 15:10:55 -0600

    Minor formatting changes
    No logic changes, just dealing with indenting, conditional blocks, etc.
    
    refs #216

--- src/js/models/metadata/eml211/EMLProject.js
+++ src/js/models/metadata/eml211/EMLProject.js
@@ -12,10 +12,10 @@ define(["jquery", "underscore", "models/NestedModel", "models/DataONEObject",
 			parentModel: null
 		},
 
-		initialize: function(options){
-			if(options && options.objectDOM)
-				this.set(this.parse(options.objectDOM));
-
+		initialize: function(options) {
+			if(options && options.objectDOM) {
+                this.set(this.parse(options.objectDOM));
+            }
 			this.on("change:personnel change:funding change:title", this.trickleUpChange);
 		},
 
@@ -30,30 +30,29 @@ define(["jquery", "underscore", "models/NestedModel", "models/DataONEObject",
 		},
 
 		//TODO: This only supports the funding and title elements right now
-		parse: function(objectDOM){
-			if(!objectDOM)
-				var objectDOM = this.get("objectDOM");
+		parse: function(objectDOM) {
+			if (!objectDOM) {
+                var objectDOM = this.get("objectDOM");
+            }
 
 			var modelJSON = {};
 
-      //Parse the title
-      var titleNode = $(objectDOM).children("title");
-      if( titleNode.length ){
-          modelJSON.title = titleNode.text() || null;
-      }
+            //Parse the title
+            var titleNode = $(objectDOM).children("title");
+            if( titleNode.length ) {
+                modelJSON.title = titleNode.text() || null;
+            }
 
 			//Parse the funding info
 			modelJSON.funding = [];
-			var fundingEl    = $(objectDOM).children("funding"),
-				  fundingNodes = fundingEl.children("para").length ? fundingEl.children("para") : fundingEl;
+			var fundingEl = $(objectDOM).children("funding"),
+				fundingNodes = fundingEl.children("para").length ? fundingEl.children("para") : fundingEl;
 
-      //Iterate over each funding node and put the text into the funding array
+            //Iterate over each funding node and put the text into the funding array
 			_.each(fundingNodes, function(fundingNode){
-
-        if( $(fundingNode).text() ){
-            modelJSON.funding.push( $(fundingNode).text() );
-        }
-
+                if( $(fundingNode).text() ) {
+                    modelJSON.funding.push( $(fundingNode).text() );
+                }
 			}, this);
 
 			/*
@@ -63,34 +62,33 @@ define(["jquery", "underscore", "models/NestedModel", "models/DataONEObject",
 				modelJSON.personnel.push( new EMLParty({ objectDOM: personnelNode[i], parentModel: this }));
 			}
 			*/
-
 			return modelJSON;
 		},
 
-		serialize: function(){
+		serialize: function() {
 			var objectDOM = this.updateDOM(),
 				xmlString = objectDOM.outerHTML;
 
-			  //Camel-case the XML
+			//Camel-case the XML
 	    	xmlString = this.formatXML(xmlString);
 
 	    	return xmlString;
 		},
 
-		updateDOM: function(){
+		updateDOM: function() {
 			var objectDOM = this.get("objectDOM") ? this.get("objectDOM").cloneNode(true) : document.createElement("project");
 
 			//Create a project title
 			//If there is no title node, create one
-			if( !$(objectDOM).find("title").length ){
+			if( !$(objectDOM).find("title").length ) {
 
 				var title = this.get("title") || this.get("parentModel").get("title") || "";
 				$(objectDOM).prepend( $(document.createElement("title")).text(title) );
 
-      }
+            }
 
 			//Create project personnel
-			if( !$(objectDOM).find("personnel").length ){
+			if( !$(objectDOM).find("personnel").length ) {
 				var personnel = this.get("personnel");
 
 				if(!personnel){
@@ -99,35 +97,28 @@ define(["jquery", "underscore", "models/NestedModel", "models/DataONEObject",
 
 					_.each(this.get("parentModel").get("creator"), function(creator){
 
-            var individualName = (typeof creator.get("individualName") == "object") ?
-                                    Object.assign({}, creator.get("individualName")) : null,
-                organizationName = creator.get("organizationName"),
-                positionName = creator.get("positionName");
+                        var individualName = (typeof creator.get("individualName") == "object") ?
+                            Object.assign({}, creator.get("individualName")) : null,
+                            organizationName = creator.get("organizationName"),
+                            positionName = creator.get("positionName");
 
 						var newPersonnel = new EMLParty({
 							role: "principalInvestigator",
 							parentModel: this,
 							type: "personnel",
 							individualName: individualName,
-              organizationName: organizationName,
-              positionName: positionName
+                            organizationName: organizationName,
+                            positionName: positionName
 						});
-
 						personnel.push(newPersonnel);
-
 						$(objectDOM).append(newPersonnel.updateDOM());
-
 					}, this);
-
 					this.trigger("change:personnel");
-
-				}
-				else{
+				} else {
 
 					_.each(this.get("personnel"), function(party){
 						$(objectDOM).append(party.updateDOM());
 					}, this);
-
 				}
 			}
 
@@ -136,32 +127,26 @@ define(["jquery", "underscore", "models/NestedModel", "models/DataONEObject",
 
 			if (this.get('funding') && this.get('funding').length > 0) {
 
-        // Create the funding element if needed
+                // Create the funding element if needed
 				if (fundingNode.length == 0) {
 
 					fundingNode = document.createElement("funding");
 					$(objectDOM).append(fundingNode);
-
 				} else {
-
 					// Clear the funding node out of all <para> child elements
-					// We only replace <paras> because <funding> is an EMLText module
+					// We only replace <para>s because <funding> is an EMLText module
 					// instance and can contain other content we don't want to remove
 					// when serializing
 					$(fundingNode).children("para").remove();
-
 				}
 
-        //Add a <para> element with the text for each funding info
+                //Add a <para> element with the text for each funding info
 				_.each(this.get('funding'), function(f) {
 					$(fundingNode).append( $(document.createElement("para")).text(f) );
 				});
-
 			} else if ( (!this.get('funding') || !this.get('funding').length) && fundingNode.length > 0) {
-
-          // Remove all funding elements
-					$(fundingNode).remove();
-
+                // Remove all funding elements
+				$(fundingNode).remove();
 			}
 
 			// Remove empty (zero-length or whitespace-only) nodes
@@ -170,32 +155,31 @@ define(["jquery", "underscore", "models/NestedModel", "models/DataONEObject",
 			return objectDOM;
 		},
 
-    /*
-    * Climbs up the model heirarchy until it finds the EML model
-    *
-    * @return {EML211 or false} - Returns the EML 211 Model or false if not found
-    */
-    getParentEML: function(){
-      var emlModel = this.get("parentModel"),
-          tries = 0;
+        /*
+        * Climbs up the model heirarchy until it finds the EML model
+        *
+        * @return {EML211 or false} - Returns the EML 211 Model or false if not found
+        */
+        getParentEML: function() {
+            var emlModel = this.get("parentModel"),
+            tries = 0;
+            while (emlModel.type !== "EML" && tries < 6) {
+                emlModel = emlModel.get("parentModel");
+                tries++;
+            }
+            
+            if ( emlModel && emlModel.type == "EML") {
+                return emlModel;
+            } else {
+                return false;
+            }
+        },
 
-      while (emlModel.type !== "EML" && tries < 6){
-        emlModel = emlModel.get("parentModel");
-        tries++;
-      }
-
-      if( emlModel && emlModel.type == "EML")
-        return emlModel;
-      else
-        return false;
-
-    },
-
-		trickleUpChange: function(){
+		trickleUpChange: function() {
 			MetacatUI.rootDataPackage.packageModel.set("changed", true);
 		},
 
-		formatXML: function(xmlString){
+		formatXML: function(xmlString) {
 			return DataONEObject.prototype.formatXML.call(this, xmlString);
 		}
 	});
