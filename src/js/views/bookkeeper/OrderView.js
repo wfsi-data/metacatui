6f3d2fc648f8dd7dbfc78364f9340b2712babd1c
laurenwalker <walker.2558@osu.edu>
2020-10-27 20:46:07 -0500

    Add basic rendering of the OrderView according to the new UI mockups

--- src/js/views/bookkeeper/OrderView.js
+++ src/js/views/bookkeeper/OrderView.js
@@ -2,8 +2,9 @@ define(["jquery",
     "underscore",
     "backbone",
     "models/bookkeeper/Order",
-    "views/portals/PortalUsagesView"],
-    function($, _, Backbone, Order, PortalUsagesView){
+    "views/portals/PortalUsagesView",
+    "text!templates/bookkeeper/order.html"],
+    function($, _, Backbone, Order, PortalUsagesView, Template){
 
     /**
     * @class OrderView
@@ -19,33 +20,7 @@ define(["jquery",
 
       tagName: "div",
 
-      template: _.template('<div class="order-view-title-container"></div>' +
-                           '<div class="order-view-status-container"></div>' +
-                           '<div class="order-view-portal-quota-container"></div>' +
-                           '<div class="my-portals-container"></div>'),
-      /**
-      * An Underscore template that displays the Order title
-      * @type {_.template}
-      */
-      titleTemplate: _.template('<h3 class="order-view-title">My <%=MetacatUI.appModel.get("dataonePlusName")%>  <%=MetacatUI.appModel.get("dataonePlusGeneralName")%>' +
-                                '<span class="subtle order-view-id"></span>' +
-                                '<h3>'),
-      /**
-      * An Underscore template that displays the Order status
-      * @type {_.template}
-      */
-      statusTemplate: _.template('<h4 class="order-view-status"><span class="text-info status-label">Status: </span><span class="order-status"><%=statusNames[status]%></span></h4>'),
-      /**
-      * An Underscore template that displays the portal quota for this Order
-      * @type {_.template}
-      */
-      portalQuotaTemplate: _.template('<h4 class="order-view-portal-quota-usage">' +
-                                      '<span class="portal-usage-count-container"></span> of <span class="portal-quota-count-container"></span>' +
-                                      ' ' + MetacatUI.appModel.get("portalTermPlural") + ' created.' +
-                                      '<div class="progress progress-info">' +
-                                      '<div class="bar"></div>' +
-                                      '</div>' +
-                                      '</h4>'),
+      template: _.template(Template),
 
       /**
       * The Order model that is displayed in this view
@@ -57,15 +32,12 @@ define(["jquery",
       * A jQuery selector for the element that the PortalUsagesView should be inserted into
       * @type {string}
       */
-      portalListContainer: ".my-portals-container",
+      portalListContainer: ".order-view-portal-usages-container",
       /**
-      * A jQuery selector for the element that the title should be inserted into
+      * A jQuery selector for the element that contains the navigation for this view
       * @type {string}
       */
-      titleContainer: ".order-view-title-container",
-      statusContainer: ".order-view-status-container",
-      portalQuotaContainer: ".order-view-portal-quota-container",
-      orderIdContainer: ".order-view-id",
+      navigationContainer: ".order-view-nav",
 
       render: function(){
 
@@ -75,8 +47,13 @@ define(["jquery",
             return;
           }
 
+          //Convert the Order model attributes to JSON
+          var templateJSON = this.order.toJSON();
+          templateJSON.expiry = "";
+          templateJSON.daysLeft = "";
+
           //Insert the template into this view
-          this.$el.html( this.template() );
+          this.$el.html( this.template(templateJSON) );
 
           //Get the portals from this Order
           var portalQuotas = this.order.getQuotas("portal");
@@ -86,35 +63,25 @@ define(["jquery",
             portalUsagesView.order = this.order;
 
             //Render the Portal list view and insert it in the page
-            portalUsagesView.render();
             this.$(this.portalListContainer).html(portalUsagesView.el);
-
-            //Add a title for this Order View. Only display this if there is a
-            // portals quota for now
-            var orderJSON = this.order.toJSON();
-            this.$(this.titleContainer).html(this.titleTemplate(orderJSON));
-            this.$(this.orderIdContainer).text(orderJSON.id);
-            this.$(this.statusContainer).html(this.statusTemplate(orderJSON));
-            this.$(this.portalQuotaContainer).html(this.portalQuotaTemplate(orderJSON));
+            portalUsagesView.render();
 
             //Get the total portal quota
-            var quotaTotal = this.order.getTotalQuotaLimit("portal");
+            var portalQuotaTotal = this.order.getTotalQuotaLimit("portal");
 
             //Get the total portal usage
-            var usageTotal = this.order.getTotalUsage("portal");
-
-            //Show the current portal usage for this order
-            this.$(".portal-usage-count-container").text( usageTotal );
-            this.$(".portal-quota-count-container").text( quotaTotal );
+            var portalUsageTotal = this.order.getTotalUsage("portal");
 
             //Remove the usage progress bar if the usage or quota is zero.
-            if( usageTotal <= 0 || quotaTotal <= 0 ){
+            if( portalUsageTotal <= 0 || portalQuotaTotal <= 0 ){
               this.$(".order-view-portal-quota-usage .progress").remove();
             }
             else{
-              this.$(".order-view-portal-quota-usage .progress .bar").css("width", (usageTotal / quotaTotal) * 100 + "%");
+              this.$(".order-view-portal-quota-usage .progress .bar").css("width", (portalUsageTotal / portalQuotaTotal) * 100 + "%");
             }
           }
+
+          //Render the People
 
         }
         catch(e){
